17fa1bbd189cc1518b4aaf6eb2f268e0ef96760d
tryniak <tiv333@mail.ru>
2017-02-19 22:49:08 +0200

    Add files via upload

--- task 5/src/AandB.java
+++ task 5/src/AandB.java
@@ -5,24 +5,25 @@ public class AandB {
     double a;
     double b;
 
-    public double div(double a, double b) {
-        double c = a / b;
-        System.out.println(" c = " + c);
-        return c;
+    public double div(double a, double b) throws ArithmeticException {
+
+        if (b == 0) throw new ArithmeticException();
+        return a / b;
     }
 
     public static void main(String[] args) {
-        AandB rectangle = new AandB();
-        Scanner sc = new Scanner(System.in);
+        AandB aandB = new AandB();
 
         try {
-            double a = sc.nextDouble();
-            double b = sc.nextDouble();
-            if (b == 0) throw new ArithmeticException();
-            rectangle.div(a, b);
+
+            System.out.println(aandB.div(5, 7));
         } catch (InputMismatchException e) {
             System.out.println("You input incorrect type. You mast input a number.");
 
         }
+catch (ArithmeticException e) {
+    System.out.println("sfs");
+}
     }
+
 }
